351327c1bae70560d00992cd2163ec569bf04652
mardemj <96274159+mardemj@users.noreply.github.com>
2022-01-06 02:25:24 -0500

    add first completed version of ui

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 "use strict";
 
+const buttons = document.querySelectorAll(".btn");
+const monitorRight = document.querySelector(".monitor-right");
+const monitorLeft = document.querySelector(".monitor-left");
+
+let playerScoreText = document.querySelector(".player-score");
+let computerScoreText = document.querySelector(".computer-score");
+let drawScoreText = document.querySelector(".draw-score")
+let message = document.querySelector(".message");
+let winMessage = document.querySelector("h3");
+let playerSelection;
 let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 
+
 function computerPlay() {
   // create an array that holds "rock", "paper", and "scissors"
-  const options = ["rock", "paper", "scissors"];
+  const options = ["ROCK", "PAPER", "SCISSORS"];
 
   // generate a random # between 0 and 2
   const randomNum = Math.floor(Math.random() * 3);
 
   // display choice on monitor
-  computerSide.textContent = options[randomNum];
+  monitorLeft.textContent = options[randomNum];
 
   // return choice from options using random number
   return options[randomNum];
@@ -20,38 +31,38 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   // transform playerSelection to lowercase
-  let psLowerCase = playerSelection.toLowerCase();
+  let psUpperCase = playerSelection.toUpperCase();
 
   // stage outcom conditions
   const draw = "Round draw, No winner!";
-  const playerLost = `You lose ${computerSelection} beats ${psLowerCase}!`;
-  const playerWin = `You won ${psLowerCase} beats ${computerSelection}!`;
+  const playerLost = `Round lost ${computerSelection} beats ${psUpperCase}!`;
+  const playerWin = `Round win ${psUpperCase} beats ${computerSelection}!`;
 
-  playerSide.textContent = psLowerCase;
+  monitorRight.textContent = psUpperCase;
 
-  if (psLowerCase === "rock" && computerSelection === "paper") {
+  if (psUpperCase === "ROCK" && computerSelection === "PAPER") {
     computerScore++;
     return playerLost;
   }
-  if (psLowerCase === "rock" && computerSelection === "scissors") {
+  if (psUpperCase === "ROCK" && computerSelection === "SCISSORS") {
     playerScore++;
     return playerWin;
   }
 
-  if (psLowerCase === "paper" && computerSelection === "rock") {
+  if (psUpperCase === "PAPER" && computerSelection === "ROCK") {
     playerScore++;
     return playerWin;
   }
-  if (psLowerCase === "paper" && computerSelection === "scissors") {
+  if (psUpperCase === "PAPER" && computerSelection === "SCISSORS") {
     computerScore++;
     return playerLost;
   }
 
-  if (psLowerCase === "scissors" && computerSelection === "paper") {
+  if (psUpperCase === "SCISSORS" && computerSelection === "PAPER") {
     playerScore++;
     return playerWin;
   }
-  if (psLowerCase === "scissors" && computerSelection === "rock") {
+  if (psUpperCase === "SCISSORS" && computerSelection === "ROCK") {
     computerScore++
     return playerLost;
   }
@@ -60,34 +71,55 @@ function playRound(playerSelection, computerSelection) {
   return draw;
 }
 
-function game(rounds) {
-  for (let i = 0; i < rounds; i++) {
-    const playerSelection = prompt("rock, paper, or scissors?");
+function checkWinner(playerScore, computerScore) {
+  if(playerScore >= 5)
+  {
+    winMessage.classList.add("winColor");
+    winMessage.textContent = "You WON the Game!!!";
+    setTimeout(restartGame, 1500);
+  }
+  else if(computerScore >= 5){
+    winMessage.classList.add("lostColor");
+    winMessage.textContent = "You LOST the Game";
+    setTimeout(restartGame, 1500);
   }
 }
 
+function restartGame() {
+  winMessage.classList.remove("winColor");
+  winMessage.classList.remove("lostColor");
+
+  monitorRight.textContent = "";
+  monitorLeft.textContent = "";
+
+  playerScore = 0;
+  computerScore = 0;
+  drawScore = 0;
+
+  playerScoreText.textContent = playerScore;
+  computerScoreText.textContent = computerScore;
+  drawScoreText.textContent = drawScore;
+
+  message.textContent = "First to 5 wins! Click RPS buttons---> ";
+}
+
+
 // add click listeners to all button
-const buttons = document.querySelectorAll(".btn");
-const playerSide = document.querySelector(".player-side");
-const computerSide = document.querySelector(".computer-side");
-
-let playerScoreText = document.querySelector(".player-score");
-let computerScoreText = document.querySelector(".computer-score");
-let drawScoreText = document.querySelector(".draw-score")
-let playerSelection;
-
 for(let i = 0; i < buttons.length; i++){
   // when the player clicks a button
   buttons[i].addEventListener("click", function() {
       // store the button's text content as the player's selection
       playerSelection = this.textContent;
 
-      playRound(playerSelection, computerPlay());
-
+      // play round and update player feedback message
+      message.textContent = playRound(playerSelection, computerPlay());
 
       // update score text
       playerScoreText.textContent = playerScore;
       computerScoreText.textContent = computerScore;
       drawScoreText.textContent = drawScore;
+
+
+      checkWinner(playerScore, computerScore);
   });
 }
